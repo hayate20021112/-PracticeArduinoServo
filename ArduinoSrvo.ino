e5579a8aac0c68ace2da6881c05e4c0f575ecfb8
IGOH <39585656+hayate20021112@users.noreply.github.com>
2021-01-08 14:13:03 +0900

    first commit

--- ArduinoSrvo.ino
+++ ArduinoSrvo.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 Servo sv;
 const int inPin = 2;
+const int MotionLED = 12;
+const int PowerLED = 13;
 
 void setup() {
     pinMode (inPin, INPUT);
+    pinMode (MotionLED, OUTPUT);
+    pinMode (PowerLED, OUTPUT);
     Serial.begin(9600);
     Serial.print("Arduino / ON \n");
+    digitalWrite(PowerLED,HIGH);
     sv.attach (3);
     sv.write (90);
     Serial.print("ServoPosition 90 \n");
@@ -14,10 +19,12 @@ void setup() {
 void loop() {
     if (digitalRead(inPin) == HIGH) {
         sv.write(0);
+        digitalWrite(MotionLED,HIGH);
         Serial.print("ServoPosition 0 \n");
         delay(2500);
     } else {
         sv.write(90);
+        digitalWrite(MotionLED,LOW);
         Serial.print("ServoPosition 90 \n");
     }
 }
