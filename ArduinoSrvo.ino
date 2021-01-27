c4ef9adf0da2e7f127ba6c839f84088336897d4e
IGOH <39585656+hayate20021112@users.noreply.github.com>
2021-01-27 21:30:25 +0900

    first commit

--- ArduinoSrvo.ino
+++ ArduinoSrvo.ino
@@ -1,15 +1,19 @@
 #include <Servo.h>
 Servo sv;
-const int inPin = 2;
+const int HIGHinPin = 5;
+const int LOWinPin = 6;
 const int Servo = 3;
 const int MotionLED = 12;
+const int LOWLED = 11;
 const int PowerLED = 13;
 
 const int angle = 63;
 
 void setup() {
-    pinMode (inPin, INPUT);
+    pinMode (HIGHinPin, INPUT);
+    pinMode (LOWinPin, INPUT);
     pinMode (MotionLED, OUTPUT);
+    pinMode (LOWLED, OUTPUT);
     pinMode (PowerLED, OUTPUT);
     Serial.begin(9600);
     Serial.print("Arduino / ON \n");
@@ -20,15 +24,19 @@ void setup() {
 }
 
 void loop() {
-    if (digitalRead(inPin) == LOW) {
+    if (digitalRead(HIGHinPin) == LOW) {
         Serial.print("ServoPosition Motion; \n");
+        digitalWrite(LOWLED, LOW);
         digitalWrite(MotionLED, HIGH);
-        delay(1000);
         sv.write(angle);
-        delay(3000);
-    } else {
-        sv.write(0);
+    } else if(digitalRead(LOWinPin) == HIGH){
+        Serial.print("ServoPosition LOW; \n");
         digitalWrite(MotionLED, LOW);
+        digitalWrite(LOWLED, HIGH);
+        delay(2500);
+        sv.write(0);
+    } else {
         Serial.print("ServoPosition 0; \n");
+        digitalWrite(LOWLED, LOW);
     }
 }
